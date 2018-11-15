36a3bedc37698b90e87c3e3a23c8aef45d97b4f4
Sersius <segoro97@msn.com>
2018-11-15 12:00:27 +0100

    Todo2

---
+++ SpeakerFeedback/app/src/main/java/edu/upc/citm/android/speakerfeedback/MainActivity.java
@@ -0,0 +1,146 @@
+package edu.upc.citm.android.speakerfeedback;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REGISTER_USER = 0;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private TextView textview;
+    private String userId;
+    private ListenerRegistration roomRegistration;
+    private ListenerRegistration usersRegistration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textview = findViewById(R.id.textview);
+        getOrRegisterUser();
+    }
+
+    private EventListener<DocumentSnapshot> roomListener = new EventListener<DocumentSnapshot>() {
+        @Override
+        public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+            if (e != null){
+                Log.e("SpeakerFeedback","Error al rebre rooms/testroom", e);
+                return;
+            }
+            String name = documentSnapshot.getString("name");
+            setTitle(name);
+        }
+    };
+
+    private EventListener<QuerySnapshot> usersListener = new EventListener<QuerySnapshot>() {
+        @Override
+        public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+            if (e != null) {
+                Log.e("SpeakerFeedback", "Error al rebre usuaris dins d'un room", e);
+                return;
+            }
+            textview.setText(String.format("Numuser: %d", documentSnapshots.size()));
+            String nomsUsuaris = "";
+            for (DocumentSnapshot doc : documentSnapshots){
+                nomsUsuaris += doc.getString("name") + "\n";
+            }
+            //textview.setText(nomsUsuaris);
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        roomRegistration = db.collection("rooms").document("testroom")
+                .addSnapshotListener(roomListener);
+
+        usersRegistration = db.collection("users").whereEqualTo("room", "testroom")
+                .addSnapshotListener(usersListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+         roomRegistration.remove();
+         usersRegistration.remove();
+    }
+
+    private void getOrRegisterUser() {
+        // Busquem a les preferències de l'app l'ID de l'usuari per saber si ja s'havia registrat
+        SharedPreferences prefs = getSharedPreferences("config", MODE_PRIVATE);
+        userId = prefs.getString("userId", null);
+        if (userId == null) {
+            // Hem de registrar l'usuari, demanem el nom
+            Intent intent = new Intent(this, RegisterUserActivity.class);
+            startActivityForResult(intent, REGISTER_USER);
+            Toast.makeText(this, "Encara t'has de registrar", Toast.LENGTH_SHORT).show();
+        } else {
+            // Ja està registrat, mostrem el id al Log
+            Log.i("SpeakerFeedback", "userId = " + userId);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REGISTER_USER:
+                if (resultCode == RESULT_OK) {
+                    String name = data.getStringExtra("name");
+                    registerUser(name);
+                } else {
+                    Toast.makeText(this, "Has de registrar un nom", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void registerUser(String name) {
+        Map<String, Object> fields = new HashMap<>();
+        fields.put("name", name);
+        db.collection("users").add(fields).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                // Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                // textview.setText(documentReference.getId());
+                userId = documentReference.getId();
+                SharedPreferences prefs = getSharedPreferences("config", MODE_PRIVATE);
+                prefs.edit()
+                        .putString("userId", userId)
+                        .commit();
+                Log.i("SpeakerFeedback", "New user: userId = " + userId);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("SpeakerFeedback", "Error creant objecte", e);
+                Toast.makeText(MainActivity.this,
+                        "No s'ha pogut registrar l'usuari, intenta-ho més tard", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+}
